e42d64c0e0fc2d9a95f00b3a198cfe60a9f040d8
michon-archeray theo <mct0923a@ens.fsi>
2022-09-26 19:29:31 +0200

    creation des classes

---
+++ src/histoire/Histoire.java
@@ -0,0 +1,15 @@
+package histoire;
+import personnages.Gaulois;
+
+public class Histoire {
+
+	public static void main(String[] args) {
+		
+		
+		Gaulois asterix = new Gaulois("Asterix",10);
+		asterix.parler("salut");
+		
+	}
+
+	
+}
---
+++ src/histoire/Village.java
@@ -0,0 +1,13 @@
+package histoire;
+
+public class Village {
+	private String nom;
+	
+	public Village (String nom) {
+		this.nom=nom;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+}
